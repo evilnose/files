66d0e6532c218f6c74709608ebb72cf3eee83bcc
Gary Geng <garygengxiao@gmail.com>
2023-01-28 18:43:18 -0600

    done with treesitter

--- init.lua
+++ init.lua
@@ -64,7 +64,6 @@ Plug 'mhinz/vim-startify'
 
 Plug 'nvim-tree/nvim-web-devicons'
 
--- Plug('nvim-treesitter/nvim-treesitter', {['do'] = vim.fn['TSUpdate']})
 Plug 'junegunn/fzf'
 Plug 'junegunn/fzf.vim'
 Plug 'numToStr/Comment.nvim'
@@ -177,49 +176,6 @@ require("nvim-tree").setup({
     },
   }
 })
--- require('nvim-treesitter.configs').setup {
---   -- A list of parser names, or "all"
---   ensure_installed = { "lua" },
---
---   -- Install parsers synchronously (only applied to `ensure_installed`)
---   sync_install = false,
---
---   -- Automatically install missing parsers when entering buffer
---   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
---   auto_install = true,
---
---   -- List of parsers to ignore installing (for "all")
---   ignore_install = { "javascript", "gitignore" },
---
---   ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
---   -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run
---   -- vim.opt.runtimepath:append("/some/path/to/store/parsers")!
---
---   highlight = {
---     -- `false` will disable the whole extension
---     enable = true,
---
---     -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
---     -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
---     -- the name of the parser)
---     -- list of language that will be disabled
---     disable = { "gitignore", "c", "cpp" },
---     -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
---     disable = function(lang, buf)
---         local max_filesize = 100 * 1024 -- 100 KB
---         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
---         if ok and stats and stats.size > max_filesize then
---             return true
---         end
---     end,
---
---     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
---     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
---     -- Using this option may slow down your editor, and you may see some duplicate highlights.
---     -- Instead of true it can also be a list of languages
---     additional_vim_regex_highlighting = false,
---   },
--- }
 
 require('tabline').setup({
     show_index = true,        -- show tab index
@@ -287,7 +243,7 @@ require('strict').setup({
 vim.api.nvim_set_keymap('n', '<Leader>r', ':lua require("replacer").run()<cr>', { nowait = true, noremap = true, silent = true })
 vim.api.nvim_create_user_command(
   'Rg',
-  'call fzf#vim#grep("/sandbox/gary.geng/usr/bin/rg --column --line-number --no-heading --color=always --smart-case ".shellescape(<q-args>), 1, fzf#vim#with_preview(), <bang>0)',
+  'call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case ".shellescape(<q-args>), 1, fzf#vim#with_preview(), <bang>0)',
   {bang = true}
 )
 
@@ -481,4 +437,3 @@ vim.api.nvim_create_autocmd("FocusGained", {
     require("tint").untint(vim.api.nvim_get_current_win())
   end
 })
-
